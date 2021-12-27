4257505f3ce17b9834afe9124612b40808163dfc
Obtez <59846143+Obtez@users.noreply.github.com>
2021-12-27 14:16:01 +0100

    updated to react-router-dom v6

---
+++ src/router/Router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "components/Layout/Layout";
+import BuilderPage from "../pages/Builder/BuilderPage"
+import PageNotFound from "../pages/PageNotFound/PageNotFound";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<BuilderPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default Router
